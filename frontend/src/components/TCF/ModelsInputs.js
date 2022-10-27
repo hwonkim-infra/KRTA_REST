d45299ca47f138f7611b4302af6a5ba041755710
hwonkim-infra <52963259+hwonkim-infra@users.noreply.github.com>
2022-10-27 22:53:06 +0900

    Modelnput Row

--- frontend/src/components/TCF/ModelsInputs.js
+++ frontend/src/components/TCF/ModelsInputs.js
@@ -1,10 +1,16 @@
-import { Grid, Paper } from "@mui/material";
-import { Checkboxes } from "mui-rff";
-import React from "react";
-const Models = ["HX60A", "HX85A", "HX140A"];
+import { Grid, Paper } from '@mui/material';
+import { Checkboxes } from 'mui-rff';
+import React from 'react';
+
+const Models1 = ['HX30A', 'HX60A', 'HX85A'];
+const Models2 = ['HX140A', 'HX150A', 'HX220A'];
 
 const ModelsInput = () => {
-  const checkboxDatas = Models.map((model) => {
+  const checkboxDatas = Models1.map((model) => {
+    return { label: model, value: model };
+  });
+
+  const checkboxDatas2 = Models2.map((model) => {
     return { label: model, value: model };
   });
 
@@ -12,12 +18,17 @@ const ModelsInput = () => {
     <>
       <div>
         <Paper style={{ padding: 16 }}>
-          <Grid container alignItems="flex-start" spacing={2}>
+          <Grid container alignItems='flex-start' spacing={2}>
             <Checkboxes
-              label="Relevant Models"
-              name="Models"
-            //   required={true}
+              name='Models'
               data={checkboxDatas}
+              sx={{ '& .MuiSvgIcon-root': { fontSize: 12 } }}
+            />
+
+            <Checkboxes
+              label=''
+              name='Models'
+              data={checkboxDatas2}
               sx={{ '& .MuiSvgIcon-root': { fontSize: 12 } }}
             />
           </Grid>
